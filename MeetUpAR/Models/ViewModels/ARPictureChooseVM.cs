eabff575ce27c8b6ce7f4dfb45a9ce7f1d2dabfb
shunhaku518 <ln.m.a.anel@gmail.com>
2025-06-21 16:50:47 +0900

    add archived MeetUpAR.zip

--- MeetUpAR/Models/ViewModels/ARPictureChooseVM.cs
+++
@@ -1,9 +0,0 @@
-﻿using MeetUpAR.Models.DataModels;
-
-namespace MeetUpAR.Models.ViewModels
-{
-    public class ARPictureChooseVM
-    {
-        public List<ARPicture> ARPictures { get; set; } = new List<ARPicture>();
-    }
-}
